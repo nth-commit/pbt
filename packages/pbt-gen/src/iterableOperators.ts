60262bbfb66c9dd98b3380fac3695cbc1ff84930
nth-commit <mfrywhite@gmail.com>
2020-09-06 21:15:28 +1200

    Basic exhaustion tests with flatMap

--- packages/pbt-gen/src/iterableOperators.ts
+++ packages/pbt-gen/src/iterableOperators.ts
@@ -20,3 +20,28 @@ export const addInfiniteStreamProtection = <T>(): OperatorFunction<T, T> => (sou
   /* istanbul ignore next */
   return maxIterations === undefined ? pipe(source) : pipe(source, crashAfter(maxIterations));
 };
+
+export class TakeWhileInclusiveIterable<TSource> extends IterableX<TSource> {
+  private _source: Iterable<TSource>;
+  private _predicate: (value: TSource, index: number) => boolean;
+
+  constructor(source: Iterable<TSource>, predicate: (value: TSource, index: number) => boolean) {
+    super();
+    this._source = source;
+    this._predicate = predicate;
+  }
+
+  *[Symbol.iterator]() {
+    let i = 0;
+    for (const item of this._source) {
+      yield item;
+      if (!this._predicate(item, i++)) {
+        break;
+      }
+    }
+  }
+}
+
+export const takeWhileInclusive = <T>(predicate: (value: T, index: number) => boolean): OperatorFunction<T, T> => (
+  source: Iterable<T>,
+): IterableX<T> => new TakeWhileInclusiveIterable<T>(source, predicate);
