64bf734705fcc3ec1b9c3377214eff910516541d
nth-commit <mfrywhite@gmail.com>
2020-09-13 07:31:08 +1200

    #45 Unit testing run interface, whilst filling in some behaviours

--- packages/pbt/src/index.ts
+++ packages/pbt/src/index.ts
@@ -1,5 +1,12 @@
 /* istanbul ignore file */
 export { Seed, Size, Gen as IGen } from 'pbt-core';
 export { integer, create, exhausted, Gen } from 'pbt-gen';
-export { property, Property, PropertyFunction, PropertyConfig, PropertyResult } from 'pbt-properties';
-export { run, RunConfig, RunResult } from './Run';
+export {
+  property,
+  Property,
+  PropertyFunction,
+  PropertyConfig,
+  PropertyResult,
+  PropertyCounterexample,
+} from 'pbt-properties';
+export { run, RunConfig, RunResult, Counterexample } from './Run';
