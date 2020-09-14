bf5957c7e1dc7a821b0002b9606f30c1a025d0b9
nth-commit <mfrywhite@gmail.com>
2020-09-15 09:08:14 +1200

    🎉 #58 pbt-core is now tested with pbt

--- packages/pbt-core/test/Seed.test.ts
+++ packages/pbt-core/test/Seed.test.ts
@@ -1,10 +1,10 @@
-import fc from 'fast-check';
-import { Seed } from '../src';
+import * as dev from '../src';
+import { assert, property, gen } from 'pbt-0.0.1';
 
 test('It is repeatable', () => {
-  fc.assert(
-    fc.property(fc.nat(), (seeder) => {
-      const seed = Seed.create(seeder);
+  assert(
+    property(gen.naturalNumber.unscaled().noShrink(), (seeder) => {
+      const seed = dev.Seed.create(seeder);
 
       expect(seed.nextInt(0, 1_000_000)).toEqual(seed.nextInt(0, 1_000_000));
     }),
@@ -12,28 +12,32 @@ test('It is repeatable', () => {
 });
 
 test('It is repeatable after splitting', () => {
-  fc.assert(
-    fc.property(fc.nat(), fc.array(fc.constantFrom('L' as const, 'R' as const)), (seeder, splitPath) => {
-      const seedFinal = splitPath.reduce((seed, splitDirection) => {
-        const [leftSeed, rightSeed] = seed.split();
-        switch (splitDirection) {
-          case 'L':
-            return leftSeed;
-          case 'R':
-            return rightSeed;
-        }
-      }, Seed.create(seeder));
+  assert(
+    property(
+      gen.naturalNumber.unscaled().noShrink(),
+      gen.array.scaleLinearly(0, 10, gen.element(['L', 'R'] as const)),
+      (seeder, splitPath) => {
+        const seedFinal = splitPath.reduce((seed, splitDirection) => {
+          const [leftSeed, rightSeed] = seed.split();
+          switch (splitDirection) {
+            case 'L':
+              return leftSeed;
+            case 'R':
+              return rightSeed;
+          }
+        }, dev.Seed.create(seeder));
 
-      expect(seedFinal.nextInt(0, 1_000_000)).toEqual(seedFinal.nextInt(0, 1_000_000));
-    }),
+        expect(seedFinal.nextInt(0, 1_000_000)).toEqual(seedFinal.nextInt(0, 1_000_000));
+      },
+    ),
   );
 });
 
 test('It can be replicated', () => {
-  fc.assert(
-    fc.property(fc.nat(), (seeder) => {
-      const seed0 = Seed.create(seeder);
-      const seed1 = Seed.create(seed0.valueOf());
+  assert(
+    property(gen.naturalNumber.unscaled().noShrink(), (seeder) => {
+      const seed0 = dev.Seed.create(seeder);
+      const seed1 = dev.Seed.create(seed0.valueOf());
 
       expect(seed0.nextInt(0, 1_000_000)).toEqual(seed1.nextInt(0, 1_000_000));
     }),
