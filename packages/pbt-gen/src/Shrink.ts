99c3a990110c097d3448974f8a20f4228b854008
nth-commit <mfrywhite@gmail.com>
2020-09-16 17:05:53 +1200

    💎 #64 Implement array shrinker

--- packages/pbt-gen/src/Shrink.ts
+++ packages/pbt-gen/src/Shrink.ts
@@ -1,10 +1,10 @@
 import { pipe, generate, concat, of, empty } from 'ix/iterable';
-import { map } from 'ix/iterable/operators';
+import { flatMap, map, skip } from 'ix/iterable/operators';
 
 export type Shrink<T> = (value: T) => Iterable<T>;
 
 export namespace Shrink {
-  const halves = (value: number, dp: number): Iterable<number> => {
+  const halves = (value: number): Iterable<number> => {
     const sign = Math.sign(value);
     const absValue = Math.abs(value);
     const end = absValue < 1 ? empty() : of(1);
@@ -23,9 +23,64 @@ export namespace Shrink {
     );
   };
 
-  export const towardsNumber = (target: number, dp: number): Shrink<number> => (value) =>
+  export const towardsNumber = (target: number): Shrink<number> => (value) =>
     pipe(
-      halves(value - target, dp),
+      halves(value - target),
       map((x) => value - x),
     );
+
+  export const combinations = <T>(k0: number): Shrink<T[]> => (arr0) => {
+    const headCombinations = function* (k: number, arr: T[]): Iterable<T[]> {
+      const [head, ...tail] = arr;
+
+      yield* pipe(
+        allCombinations(k - 1, tail),
+        map((tailCombinations) => [head, ...tailCombinations]),
+      );
+    };
+
+    const allCombinations = function* (k: number, arr: T[]): Iterable<T[]> {
+      if (k === 1) yield* arr.map((x) => [x]);
+      else {
+        const n = arr.length;
+        for (let i = 0; i < n; i++) {
+          yield* headCombinations(k, arr.slice(i));
+        }
+      }
+    };
+
+    if (k0 === arr0.length) return empty();
+    if (k0 === 0) return empty();
+    return allCombinations(k0, arr0);
+  };
+
+  /**
+   * Shrinks an array by edging from the smallest possible array towards the given value. Shrinks via two passes. The
+   * first pass simply drops elements from the end of the original array. The second pass generates arrays of the
+   * same lengths, but will attempt all possible combinations at each length.
+   */
+  export const array = <T>(): Shrink<T[]> => (arr) => {
+    const { length } = arr;
+
+    const shrunkLengths = pipe(
+      halves(length - 0),
+      map((dropCount) => length - dropCount),
+    );
+
+    return concat<T[], T[]>(
+      pipe(
+        shrunkLengths,
+        map((l) => arr.slice(0, l)),
+      ),
+      pipe(
+        shrunkLengths,
+        flatMap((l) =>
+          pipe(
+            combinations<T>(l)(arr),
+            skip(1), // Already have this combination from the first pass
+          ),
+        ),
+      ),
+    );
+  };
 }
--- packages/pbt-gen/test/Shrink.test.ts
+++ packages/pbt-gen/test/Shrink.test.ts
@@ -1,15 +1,77 @@
 import { Shrink } from '../src/Shrink';
 
 test.each([
-  { target: 0, dp: 0, value: 0, expectedShrinks: [] },
-  { target: 0, dp: 0, value: 100, expectedShrinks: [0, 50, 75, 87, 93, 96, 98, 99] },
-  { target: 0, dp: 0, value: 200, expectedShrinks: [0, 100, 150, 175, 187, 193, 196, 198, 199] },
-  { target: 10, dp: 0, value: 100, expectedShrinks: [10, 55, 77, 88, 94, 97, 98, 99] },
-  { target: -10, dp: 0, value: 100, expectedShrinks: [-10, 45, 72, 86, 93, 96, 98, 99] },
-  { target: 0, dp: 0, value: -100, expectedShrinks: [0, -50, -75, -87, -93, -96, -98, -99] },
-  { target: -10, dp: 0, value: 0, expectedShrinks: [-10, -5, -3, -2, -1] },
-])('towardsNumber', ({ target, dp, value, expectedShrinks }) => {
-  const shrinker = Shrink.towardsNumber(target, dp);
+  { target: 0, value: 0, expectedShrinks: [] },
+  { target: 0, value: 100, expectedShrinks: [0, 50, 75, 87, 93, 96, 98, 99] },
+  { target: 0, value: 200, expectedShrinks: [0, 100, 150, 175, 187, 193, 196, 198, 199] },
+  { target: 10, value: 100, expectedShrinks: [10, 55, 77, 88, 94, 97, 98, 99] },
+  { target: -10, value: 100, expectedShrinks: [-10, 45, 72, 86, 93, 96, 98, 99] },
+  { target: 0, value: -100, expectedShrinks: [0, -50, -75, -87, -93, -96, -98, -99] },
+  { target: -10, value: 0, expectedShrinks: [-10, -5, -3, -2, -1] },
+])('towardsNumber', ({ target, value, expectedShrinks }) => {
+  const shrinker = Shrink.towardsNumber(target);
+
+  const shrinks = Array.from(shrinker(value));
+
+  expect(shrinks).toEqual(expectedShrinks);
+});
+
+test.each([
+  { value: [], size: 0, expectedShrinks: [] },
+  { value: [], size: 1, expectedShrinks: [] },
+  { value: ['a'], size: 0, expectedShrinks: [] },
+  { value: ['a', 'b'], size: 0, expectedShrinks: [] },
+  { value: ['a', 'b'], size: 1, expectedShrinks: [['a'], ['b']] },
+  { value: ['a', 'b', 'c'], size: 1, expectedShrinks: [['a'], ['b'], ['c']] },
+  {
+    value: ['a', 'b', 'c'],
+    size: 2,
+    expectedShrinks: [
+      ['a', 'b'],
+      ['a', 'c'],
+      ['b', 'c'],
+    ],
+  },
+  {
+    value: ['a', 'b', 'c', 'd'],
+    size: 2,
+    expectedShrinks: [
+      ['a', 'b'],
+      ['a', 'c'],
+      ['a', 'd'],
+      ['b', 'c'],
+      ['b', 'd'],
+      ['c', 'd'],
+    ],
+  },
+  {
+    value: ['a', 'b', 'c', 'd'],
+    size: 3,
+    expectedShrinks: [
+      ['a', 'b', 'c'],
+      ['a', 'b', 'd'],
+      ['a', 'c', 'd'],
+      ['b', 'c', 'd'],
+    ],
+  },
+])('combinations', ({ value, size, expectedShrinks }) => {
+  const shrinker = Shrink.combinations(size);
+
+  const shrinks = Array.from(shrinker(value));
+
+  expect(shrinks).toEqual(expectedShrinks);
+});
+
+test.each([
+  { value: [], expectedShrinks: [] },
+  { value: ['a'], expectedShrinks: [[]] },
+  { value: ['a', 'b'], expectedShrinks: [[], ['a'], ['b']] },
+  {
+    value: ['a', 'b', 'c'],
+    expectedShrinks: [[], ['a'], ['a', 'b'], ['b'], ['c'], ['a', 'c'], ['b', 'c']],
+  },
+])('array', ({ value, expectedShrinks }) => {
+  const shrinker = Shrink.array();
 
   const shrinks = Array.from(shrinker(value));
 
