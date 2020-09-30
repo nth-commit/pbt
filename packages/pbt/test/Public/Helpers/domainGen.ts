ae3674bf4934c3ef475820cf480278466566de13
nth-commit <mfrywhite@gmail.com>
2020-09-30 13:40:28 +1300

    🚧 #78 Make some tests for "check" generic

--- packages/pbt/test/Public/Helpers/domainGen.ts
+++ packages/pbt/test/Public/Helpers/domainGen.ts
@@ -5,3 +5,8 @@ export { func, gen, gens, fallibleFunc, infallibleFunc } from '../../helpers/dom
 export const seed = (): fc.Arbitrary<number> => fc.nat().noShrink();
 
 export const size = (): fc.Arbitrary<number> => fc.oneof(fc.integer(0, 100), fc.constant(0), fc.constant(100));
+
+export const counterexamplePath = (): fc.Arbitrary<string> => fc.array(fc.nat(), 0, 10).map((arr) => arr.join(':'));
+
+export const maybe = <T>(gen: fc.Arbitrary<T>): fc.Arbitrary<T | undefined> =>
+  fc.frequency({ weight: 3, arbitrary: gen }, { weight: 1, arbitrary: fc.constant(undefined) });
