3c93bdba5e774274f34b3205c22557933cf88f54
nth-commit <mfrywhite@gmail.com>
2020-11-09 23:18:50 +1300

    💎 #134 Fixes of jest integration
    - pbt is a peer dependency, so it should use the same global conf
    - also augment test.skip, test.only, and others

--- packages/pbt/e2e/ShrinkingChallenges.test.ts
+++ packages/pbt/e2e/ShrinkingChallenges.test.ts
@@ -1,42 +1,38 @@
-import fc from 'fast-check';
 import { max } from 'ix/iterable';
-import { assert, property } from 'pbt-vnext';
 import * as dev from '../src';
 import * as domainGen from './domainGen';
 
-test('Fallacy: the array is in reverse order', () => {
-  // Based on: https://github.com/jlink/shrinking-challenge/blob/main/challenges/reverse.md
-  assert(
-    property(domainGen.seed(), domainGen.size(), (seed, size) => {
-      const g = dev.Gen.integer().array();
-      const m = dev.minimal(
-        g,
-        (xs) => {
-          const xs0 = [...xs].sort((a, b) => b - a);
-          return xs.every((x, i) => x === xs0[i]);
-        },
-        { seed, size },
-      );
+// Based on: https://github.com/jlink/shrinking-challenge/blob/main/challenges/reverse.md
+test.property('Fallacy: the array is in reverse order', domainGen.seed(), domainGen.size(), (seed, size) => {
+  const g = dev.Gen.integer().array();
 
-      expect([
-        [0, 1],
-        [-1, 0],
-      ]).toContainEqual(m);
-    }),
+  const m = dev.minimal(
+    g,
+    (xs) => {
+      const xs0 = [...xs].sort((a, b) => b - a);
+      return xs.every((x, i) => x === xs0[i]);
+    },
+    { seed, size },
   );
+
+  expect([
+    [0, 1],
+    [-1, 0],
+  ]).toContainEqual(m);
 });
 
-test('Fallacy: the array does not contain a value >= 900', () => {
-  // Based on: https://github.com/jlink/shrinking-challenge/blob/main/challenges/lengthlist.md
-  assert(
-    property(domainGen.seed(), domainGen.size(), (seed, size) => {
-      const g = dev.Gen.integer()
-        .between(1, 100)
-        .flatMap((length) => dev.Gen.integer().between(0, 1000).array().ofLength(length));
+// Based on: https://github.com/jlink/shrinking-challenge/blob/main/challenges/lengthlist.md
+test.property(
+  'Fallacy: the array does not contain a value >= 900',
+  domainGen.seed(),
+  domainGen.size(),
+  (seed, size) => {
+    const g = dev.Gen.integer()
+      .between(1, 100)
+      .flatMap((length) => dev.Gen.integer().between(0, 1000).array().ofLength(length));
 
-      const m = dev.minimal(g, (xs) => max(xs) < 900, { seed, size });
+    const m = dev.minimal(g, (xs) => max(xs) < 900, { seed, size });
 
-      expect(m).toEqual([900]);
-    }),
-  );
-});
+    expect(m).toEqual([900]);
+  },
+);
